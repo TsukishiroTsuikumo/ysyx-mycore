4adaf4f6b1cba2c070b501154c8a4d2f388a7e86
TsukishiroTsuikumo <2657413849@qq.com>
2026-04-02 18:06:29 +0800

    To be continue......

--- controller/rename.v
+++ controller/rename.v
@@ -4,11 +4,21 @@ module rename (
     input   [4:0]   rd_addr,
     output  [4:0]   rs1_rename_addr,
     output  [4:0]   rs2_rename_addr,
-    output  [4:0]   rd_rename_addr
+    output  [4:0]   rd_rename_addr,
+
+    input  [31:0]  rd_value_wb,
+    input  [4:0]   rd_addr_in_wb,
+    output [31:0]  w1_value,
+    output         w1_en,
+    output  [4:0]  w1_addr
 );
 
     assign rs1_rename_addr = rs1_addr;
     assign rs2_rename_addr = rs2_addr;
     assign rd_rename_addr  = rd_addr;
+
+    assign w1_value = rd_value_wb;
+    assign w1_en = 1'b1;
+    assign w1_addr = rd_addr_in_wb;
 
 endmodule
